9e12e587f61a965d80aab597a65b3f6bb3d12f24
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-03-22 15:31:34 +0000

    課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ def new
   end
 
   def index
-    @Books = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -11,15 +12,41 @@ def new
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @books = Book.all 
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id) 
+    else 
+    @books = Book.all
+      # flash.now[:notice] = "投稿に失敗しました。"
+    render :edit, status: :unprocessable_entity
+   end
   end
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+      # 4. フラッシュメッセージを定義し、new.html.erbを描画する
+      @books = Book.all
+      # flash.now[:notice] = "投稿に失敗しました。"
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id]) # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
   end
 
   private
